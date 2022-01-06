a3b2dc24d43de6aa359ee9c1f94da5a823322cbb
junghee.kim <wjdgml3834@naver.com>
2022-01-06 23:01:44 +0900

    💄  h1 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>Begin Project</h1>
+      <h1 className={styles.title}>Begin Project</h1>
       <Button text={"Click"} />
     </div>
   );
