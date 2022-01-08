3a4479d7e9661ab039f21a84298963419d06b44c
junghee.kim <wjdgml3834@naver.com>
2022-01-08 10:16:30 +0900

    ✨ useEffect 두번째 인수 실습

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => {
     setValue((prev) => prev + 1);
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
   };
   console.log("나는 계속 리렌더 될때마다 실행됨");
 
@@ -14,8 +19,29 @@ function App() {
     console.log("나는 리렌더 되어도 한번만 실행될거야");
   }, []);
 
+  // console.log("Search for", keyword);
+  // 문제가 뭐냐면 지금 키워드 검색할때만 나타나고 싶은데,
+  // 리액트는 state가 변화되면 모든 리렌더 하기에, 클릭 버튼 눌러도 콘솔에 찍힌다
+  // 나는 특정 state만 변할때를 감지하고 싶은 것이다.
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
+
+  // 이제 []안에 들어가는 키워드만 변화할때 코드가 실행될 것이다.
+  // 그래서 빈 array []를 해주었을때 한번 만 실행되는 이유이다. ->지켜볼것이 없기에 한번만 실행된다.
+  // 위에 코드 같은 경우는 처음이랑, 글자 5가 넘을때만 검색하게 해줌
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>클릭해주세요</button>
     </div>
