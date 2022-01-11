49f28ae3b0479e7e6fe3dfb5fd692860731cb262
junghee.kim <wjdgml3834@naver.com>
2022-01-11 14:18:37 +0900

    ✨  API 연동

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div>movie</div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <img src={movie.medium_cover_image} alt={movie.title} />
+                <h2>{movie.title}</h2>
+                <p>{movie.summary}</p>
+                <ul>
+                  {movie.genres.map((genre, index) => (
+                    <li key={index}>{genre}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 export default App;
