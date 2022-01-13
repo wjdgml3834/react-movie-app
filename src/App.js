f3cbfada2824501cc338f611cea46b73a016a4e3
junghee.kim <wjdgml3834@naver.com>
2022-01-14 06:59:45 +0900

    🚚  Router 폴더 생성 및 Home 이동

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genre={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Home />;
 }
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,5 @@
+function detail() {
+  return <h1>Detail</h1>;
+}
+
+export default detail;
