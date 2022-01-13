54255eba1ee227fe7fe2e4bead1b3ad5cc12d5d1
junghee.kim <wjdgml3834@naver.com>
2022-01-13 14:29:57 +0900

    🚚  Movie 컴포넌트 components 폴더로 이동

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
