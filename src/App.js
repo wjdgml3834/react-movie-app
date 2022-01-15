2cf456919588cbddcec11d99a26899bebd00dfec
junghee.kim <wjdgml3834@naver.com>
2022-01-16 00:15:54 +0900

    ✨ Movie 컴포넌트 title 링크 구현

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/detail">
+        <Route path="/movie">
           <Detail />
         </Route>
         <Route path="/">
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+// Link는 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트이다.
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres && genres.map((genre, index) => <li key={index}>{genre}</li>)}
